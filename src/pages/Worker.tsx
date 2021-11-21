93e586b648cb62229ff46fb5a6a2d5871512e3a2
dirac <78232235+diracduran@users.noreply.github.com>
2021-11-21 20:55:24 +0300

    FINALLY

--- src/pages/Worker.tsx
+++ src/pages/Worker.tsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import DatePicker, { registerLocale } from "react-datepicker";
 import ru from 'date-fns/locale/ru';
 import "react-datepicker/src/stylesheets/datepicker.scss";
-import { useHistory } from "react-router-dom";
-import { Props } from '../types';
+import { useHistory, useParams } from "react-router-dom";
+import { Props, Workers } from '../types';
 
 const Worker = (props:Props) => {
 
     registerLocale('ru', ru)
+
+    const params = useParams<Workers>();
+    const current = params.id;
+    const workerToShow = props.store!.workersList.find((worker) => worker.id === current);
 
     const [birthDate, setBirthDate] = useState(new Date());
 
@@ -16,25 +20,25 @@ const Worker = (props:Props) => {
 
     return (
         <>
-            {props.store!.workersList.map((worker) => {
-                // if(window.location.pathname === worker.id) {
-                    return (
-                        <table key={worker.id} id={worker.id}>
+            {/* {props.store!.workersList.map((worker) => {
+                if(window.location.pathname === worker.id) {
+                    return ( */}
+                        <table>
                             <tr>
                                 <th>Фамилия: </th>
-                                <td>{worker.lastName}</td>
+                                <td>{workerToShow!.lastName}</td>
                             </tr>
                             <tr>
                                 <th>Имя: </th>
-                                <td>{worker.middleName}</td>
+                                <td>{workerToShow!.middleName}</td>
                             </tr>
                             <tr>
                                 <th>Отчество: </th>
-                                <td>{worker.lastName}</td>
+                                <td>{workerToShow!.lastName}</td>
                             </tr>
                             <tr>
                                 <th>Телефон: </th>
-                                <td>{worker.phone}</td>
+                                <td>{workerToShow!.phone}</td>
                             </tr>
                             <tr>
                                 <th>Дата рождения: </th>
@@ -44,25 +48,25 @@ const Worker = (props:Props) => {
                             </tr>
                             <tr>
                                 <th>Эл. почта: </th>
-                                <td>{worker.email}</td>
+                                <td>{workerToShow!.email}</td>
                             </tr>
                             <tr>
                                 <th>Кол-во отраб. дней: </th>
-                                <td>{worker.workedDays}</td>
+                                <td>{workerToShow!.workedDays}</td>
                             </tr>
                             <tr>
                                 <th>Зарплатн. ставка на день: </th>
-                                <td>{worker.salaryPerDay}</td>
+                                <td>{workerToShow!.salaryPerDay}</td>
                             </tr>
                             <tr>
                                 <th>Зарплата: </th>
-                                <td>{Number(worker.workedDays) * Number(worker.salaryPerDay)}</td>
+                                <td>{Number(workerToShow!.workedDays) * Number(workerToShow!.salaryPerDay)}</td>
                             </tr> 
                         </table>
 
-                    )
-                // }
-            })}
+                    {/* )
+                }
+            })} */}
         
             <div>
                 <button onClick={() => history.push('/')}>🔙Назад к таблице</button>
